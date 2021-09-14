645390615fe616f51a91d34784cff51ea6fbe768
rlugtu <ryanlugtu@gmail.com>
2021-09-13 22:07:06 -0400

    first

---
+++ src/components/Grid.js
@@ -0,0 +1,53 @@
+const Grid = () => {
+    
+    return (
+        <div className="artContainer">
+        <div className="artWork">
+          <img src={require("../assets/artwork.jpg").default} alt="test" />
+        </div>
+        <div className="artWork">
+          <img src={require("../assets/artwork.jpg").default} alt="test" />
+        </div>
+        <div className="artWork">
+          <img src={require("../assets/artwork.jpg").default} alt="test" />
+        </div>
+        <div className="artWork">
+          <img src={require("../assets/artwork.jpg").default} alt="test" />
+        </div>
+        <div className="artWork">
+          <img src={require("../assets/artwork.jpg").default} alt="test" />
+        </div>
+        <div className="artWork">
+          <img src={require("../assets/artwork.jpg").default} alt="test" />
+        </div>
+        <div className="artWork">
+          <img src={require("../assets/artwork.jpg").default} alt="test" />
+        </div>
+        <div className="artWork">
+          <img src={require("../assets/artwork.jpg").default} alt="test" />
+        </div>
+        <div className="artWork">
+          <img src={require("../assets/artwork.jpg").default} alt="test" />
+        </div>
+        <div className="artWork">
+          <img src={require("../assets/artwork.jpg").default} alt="test" />
+        </div>
+        <div className="artWork">
+          <img src={require("../assets/artwork.jpg").default} alt="test" />
+        </div>
+        <div className="artWork">
+          <img src={require("../assets/artwork.jpg").default} alt="test" />
+        </div>
+        <div className="artWork">
+          <img src={require("../assets/artwork.jpg").default} alt="test" />
+        </div>
+        <div className="artWork">
+          <img src={require("../assets/artwork.jpg").default} alt="test" />
+        </div>
+        <div className="artWork">
+          <img src={require("../assets/artwork.jpg").default} alt="test" />
+        </div>
+      </div>
+    )
+}
+export default Grid
